fadd715c84b8b3db78f2b4d56fef31c43ff4fea0
M-Chukwukelu <chukwukelumarvelous@gmail.com>
2025-04-07 19:58:45 -0600

    Notificantion Routes + Backend DONE!

---
+++ backend/controllers/notification.controllers.js
@@ -0,0 +1,53 @@
+import Notification from "../models/notification.models.js";
+
+export const getNotifications = async (req, res) => {
+	try {
+		const userId = req.user._id;
+
+		const notifications = await Notification.find({ to: userId }).populate({
+			path: "from",
+			select: "username profileImg",
+		});
+
+		await Notification.updateMany({ to: userId }, { read: true });
+
+		res.status(200).json(notifications);
+	} catch (error) {
+		console.log("Error in getNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const deleteNotifications = async (req, res) => {
+	try {
+		const userId = req.user._id;
+
+		await Notification.deleteMany({ to: userId });
+
+		res.status(200).json({ message: "Notifications deleted successfully" });
+	} catch (error) {
+		console.log("Error in deleteNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const deleteNotification = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const notificationId = req.params.id;
+
+    const notification = await Notification.findOneAndDelete({
+      to: userId,
+      _id: notificationId,
+    });
+
+    if (!notification) {
+      return res.status(404).json({ message: "Notification not found" });
+    }
+
+    res.status(200).json({ message: "Notification deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteNotification function", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import {v2 as cloudinary} from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js"; 
+import notificationRoutes from "./routes/notification.routes.js";
 
 // Database connection
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -31,7 +32,8 @@ app.use(cookieParser()); //Middleware to parse cookies
 // RouteS Being Used
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes); // Assuming you have a posts route
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
