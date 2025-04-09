712487ed402577db9d0db0e8d1dc8b93af33f224
M-Chukwukelu <chukwukelumarvelous@gmail.com>
2025-04-08 22:51:28 -0600

    UI Initial MockUp Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,33 @@
-function App() {
-  return (
-    <>
-      <button className="btn btn-soft">Default</button>
-      <button className="btn btn-soft btn-primary">Primary</button>
-      <button className="btn btn-soft btn-secondary">Secondary</button>
-      <button className="btn btn-soft btn-accent">Accent</button>
-      <button className="btn btn-soft btn-info">Info</button>
-      <button className="btn btn-soft btn-success">Success</button>
-      <button className="btn btn-soft btn-warning">Warning</button>
-      <button className="btn btn-soft btn-error">Error</button>
-    </>
-  )
-}
+import { Navigate, Route, Routes } from "react-router-dom";
 
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LogInPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+import { Toaster } from "react-hot-toast";
+// import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
+function App() {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{/* A common component since it is outside the routes */}
+			< Sidebar /> 
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+			<RightPanel />
+		</div>
+	);
+}
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,31 @@
+
 import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
 
-  daisyui: {
-    themes: [
-      "light",
-    ],
-  },
-}
-
+	daisyui: {
+		themes: [
+			"light",
+			{
+				flava: {
+					...daisyUIThemes["light"],
+					"primary-content": "#ffffff",
+          secondary: "#e8dcff",
+          accent: "#beaae6",
+          neutral: "#f9f6ff",
+          "base-100": "#ffffff",
+          info: "#93c5fd",
+          success: "#34d399",
+          warning: "#facc15",
+          error: "#f87171",
+				},
+			},
+		],
+	},
+};
